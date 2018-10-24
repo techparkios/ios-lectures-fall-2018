e81e1fd46ceb40d5c1241da3d7b714c69bf5c2b0
dtaraev <dtaraev@gmail.com>
2018-10-24 19:31:20 +0300

    Добавлена лекция 5 с примером

---
+++ 05/TableViewSwift/TableViewSwift/CellModel.swift
@@ -0,0 +1,13 @@
+//
+//  CellModel.swift
+//  TableViewSwift
+//
+//  Created by d.taraev on 14.03.2018.
+//  Copyright © 2018 mail.ru. All rights reserved.
+//
+
+import Foundation
+
+class CellModel {
+    var title: String?
+}
