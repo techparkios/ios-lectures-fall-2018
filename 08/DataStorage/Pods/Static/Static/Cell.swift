0ebfe7fa44a156f724669e37ac71dc60ab7a6123
dtaraev <dtaraev@gmail.com>
2018-11-14 18:08:18 +0300

    Лекция 7 и пример

---
+++ 08/DataStorage/Pods/Static/Static/Cell.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+public protocol Cell: class {
+    static func description() -> String
+    static func nib() -> UINib?
+
+    func configure(row: Row)
+}
+
+extension Cell {
+    public static func nib() -> UINib? {
+        return nil
+    }
+}
+
+extension Cell where Self: UITableViewCell {
+    public func configure(row: Row) {
+        textLabel?.text = row.text
+        detailTextLabel?.text = row.detailText
+        imageView?.image = row.image
+        accessoryType = row.accessory.type
+        accessoryView = row.accessory.view
+    }
+}
